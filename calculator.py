c905a4dcaeb45a7591663060bc374ec853d49578
corytu <corytu@users.noreply.github.com>
2020-02-06 15:58:37 +0800

    Handle file dependency out of loop

--- calculator.py
+++ calculator.py
@@ -1,19 +1,23 @@
-from datetime import date, datetime
+import os
+from datetime import date
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
 def calculate(onboard_date=date(2018, 1, 8), leave_rule=[10, 10, 10, 14, 14, 15, 15, 15, 15, 15, 16, 17, 18, 19, 20, 21]):
+    if not os.path.isfile("leave.csv"):
+        with open("leave.csv", "w") as f:
+            f.write("off_date,period")
+    leave_records = pd.read_csv("leave.csv")
+    leave_records["off_date"] = leave_records["off_date"].apply(lambda x: date.fromisoformat(x))
     while True:
-        leave_records = pd.read_csv("leave.csv")
-        leave_records["off_date"] = leave_records["off_date"].apply(lambda x: date.fromisoformat(x))
         mission = input(
             """
             Choose one mission:
             a) Show how many leave days you can use now
             b) Add a leave record
             c) List all leave records that have been made
-            q) Exit
+            q) Quit and save changes
             """
         ).lower()
         if mission not in {"a", "b", "c", "q"}:
@@ -37,7 +41,7 @@ def calculate(onboard_date=date(2018, 1, 8), leave_rule=[10, 10, 10, 14, 14, 15,
                 off_start = input("On what date does your vacation start? (yyyy-mm-dd)\n")
                 try:
                     # Making sure the date format is correct
-                    off_start = datetime.strptime(off_start, "%Y-%m-%d").strftime("%Y-%m-%d")
+                    off_start = date.fromisoformat(off_start)
                     break
                 except ValueError:
                     print("Unexpected date format")
@@ -55,11 +59,11 @@ def calculate(onboard_date=date(2018, 1, 8), leave_rule=[10, 10, 10, 14, 14, 15,
             leave_records = leave_records.append(
                 pd.DataFrame({"off_date": [off_start], "period": [off_period]}), ignore_index=True
             )
-            leave_records.to_csv("leave.csv", index=False)
             print(leave_records)
         elif mission == "c":
             print(leave_records)
         else:
+            leave_records.to_csv("leave.csv", index=False)
             break
 
 if __name__ == "__main__":
